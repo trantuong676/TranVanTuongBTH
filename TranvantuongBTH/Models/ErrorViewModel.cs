4e503a15e150902df6651a73be516216865f5b3c
trantuong21 <trantuong21022001@gmail.com>
2023-03-21 17:38:35 +0700

    21/3

---
+++ TranvantuongBTH/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TranvantuongBTH.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
